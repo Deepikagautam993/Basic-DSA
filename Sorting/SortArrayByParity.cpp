97c356624c574378629f164321fd06590b3cbf54
Deepikagautam993 <gautamdeepika707@gmail.com>
2026-04-05 12:17:48 +0530

    Add files via upload

---
+++ Sorting/SortArrayByParity.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+vector<int> sortArrayByParity(vector<int>& nums) {
+
+    int j = 0;
+
+    for(int i = 0; i < nums.size(); i++) {
+
+        if(nums[i] % 2 == 0) {
+            swap(nums[i], nums[j]);
+            j++;
+        }
+    }
+
+    return nums;
+}
+
+int main(){
+	vector<int> nums = {3, 5, 7, 2};
+	vector<int> result = sortArrayByParity(nums);
+	
+	for(int num : result){
+		cout << num << " ";
+	}
+}
