0afc8f46cf95528db7a12779a5b1c1227741ec0b
Deepikagautam993 <gautamdeepika707@gmail.com>
2026-04-08 09:28:41 +0530

    Add files via upload

---
+++ Sorting/FirstBadVersion.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+// Test purpose: first bad version = 4
+bool isBadVersion(int version) {
+    if(version >= 4)
+        return true;
+    else
+        return false;
+}
+
+int firstBadVersion(int n) {
+    int low = 1;
+    int high = n;
+    
+    while(low < high) {
+        
+        int mid = low + (high - low) / 2;
+        
+        if(isBadVersion(mid)) {
+            high = mid;
+        }
+        else {
+            low = mid + 1;
+        }
+    }
+    return low;
+}
+
+int main() {
+    int n = 5;
+    cout << firstBadVersion(n);
+}
