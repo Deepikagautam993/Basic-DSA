5a5129839d438f47f680d1da97d97b6a0545f1fa
Deepikagautam993 <gautamdeepika707@gmail.com>
2026-04-08 09:27:25 +0530

    Add files via upload

---
+++ Sorting/Search in Rotated Sorted Array2.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+bool search(vector<int>& nums, int target) {
+    int low = 0, high = nums.size() - 1;
+
+    while(low <= high){
+        int mid = low + (high - low)/2;
+
+        if(nums[mid] == target) return true;
+
+            // Handle duplicates
+        if(nums[low] == nums[mid] && nums[mid] == nums[high]){
+            low++;
+            high--;
+        }
+        
+            // Left half sorted
+        else if(nums[low] <= nums[mid]){
+            if(nums[low] <= target && target < nums[mid])
+                high = mid - 1;
+            else
+                low = mid + 1;
+        }
+        
+            // Right half sorted
+        else{
+            if(nums[mid] < target && target <= nums[high])
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+    }
+    return false;
+}
+
+int main(){
+	vector<int> nums = {2,5,6,0,0,1,2};
+	int target = 0;
+	
+	bool found = search(nums, target);
+    
+    if(found)
+    cout << "Target Found" << endl;
+    
+    else
+    cout << "Target Not Found" << endl;
+}
