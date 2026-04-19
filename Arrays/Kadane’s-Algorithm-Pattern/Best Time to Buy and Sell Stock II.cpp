b978e8592ebf19bbd8501d3168454ed5356f2c5e
Deepikagautam993 <gautamdeepika707@gmail.com>
2026-04-19 13:53:43 +0530

    Add files via upload

---
+++ Arrays/Kadane’s-Algorithm-Pattern/Best Time to Buy and Sell Stock II.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    vector<int> prices = {7, 1, 5, 3, 6, 4};
+
+    int profit = 0;
+
+    for (int i = 1; i < prices.size(); i++) {
+
+        if (prices[i] > prices[i - 1]) {
+            profit += prices[i] - prices[i - 1];
+        }
+    }
+
+    cout << "Maximum Profit: " << profit;
+
+    return 0;
+}
