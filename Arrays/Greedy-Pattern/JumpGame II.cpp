6bbb311fa3761fb8d89b67ec1be385250f666ea0
Deepikagautam993 <gautamdeepika707@gmail.com>
2026-04-22 14:45:02 +0530

    Add files via upload

---
+++ Arrays/Greedy-Pattern/JumpGame II.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int jump(vector<int>& nums) {
+    int jumps = 0;
+    int currentEnd = 0;
+    int farthest = 0;
+
+    for(int i = 0; i < nums.size() - 1; i++) {
+        farthest = max(farthest, i + nums[i]);
+
+        if(i == currentEnd) {
+            jumps++;
+            currentEnd = farthest;
+        }
+    }
+
+    return jumps;
+}
+
+int main() {
+    vector<int> nums = {2, 3, 1, 1, 4};
+
+    int result = jump(nums);
+
+    cout << "Minimum jumps: " << result;
+
+    return 0;
+}
