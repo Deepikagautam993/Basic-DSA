76d9c753c62fc34bbd7cf5b9574eac1cd9b28550
Deepikagautam993 <gautamdeepika707@gmail.com>
2026-04-25 20:44:36 +0530

    Add files via upload

---
+++ Arrays/Matrix,2D-Pattern/Matrix Diagonal Sum.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int diagonalSum(vector<vector<int>>& mat) {
+    int n = mat.size();
+    int sum = 0;
+
+    for(int i = 0; i < n; i++) {
+        // Primary diagonal
+        sum += mat[i][i];
+
+        // Secondary diagonal
+        if(i != n - 1 - i) {
+            sum += mat[i][n - 1 - i];
+        }
+    }
+
+    return sum;
+}
+
+int main() {
+    vector<vector<int>> mat = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+
+    cout << diagonalSum(mat);
+
+    return 0;
+}
