7ac5cd23d0b741c84dcc30544aff8ee2b3f767c9
Deepikagautam993 <gautamdeepika707@gmail.com>
2026-05-07 10:41:26 +0530

    Add files via upload

---
+++ String/Valid_Palindrome II.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool checkPalindrome(string s, int left, int right) {
+
+    while(left < right) {
+
+        if(s[left] != s[right]) {
+            return false;
+        }
+
+        left++;
+        right--;
+    }
+
+    return true;
+}
+
+bool validPalindrome(string s) {
+
+    int left = 0;
+    int right = s.size() - 1;
+
+    while(left < right) {
+
+        if(s[left] != s[right]) {
+
+            return checkPalindrome(s, left + 1, right) ||
+                   checkPalindrome(s, left, right - 1);
+        }
+
+        left++;
+        right--;
+    }
+
+    return true;
+}
+
+int main() {
+
+    string s = "abca";
+
+    if(validPalindrome(s)) {
+        cout << "True";
+    }
+    else {
+        cout << "False";
+    }
+
+    return 0;
+}
